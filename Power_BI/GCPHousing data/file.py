a07c57873b6856ade7a3698909d6df517da57593
Shijin Mohanan Sheela <getshijin@gmail.com>
2025-08-25 20:54:04 +0200

    17 commit

--- Power_BI/GCPHousing data/file.py
+++ Power_BI/GCPHousing data/file.py
@@ -1,6 +1,6 @@
 import kagglehub
 
 # Download latest version
-path = kagglehub.dataset_download("sukhmandeepsinghbrar/housing-price-dataset")
+path = kagglehub.dataset_download("martinfrederiksen/danish-residential-housing-prices-1992-2024")
 
 print("Path to dataset files:", path)
